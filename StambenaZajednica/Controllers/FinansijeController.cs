fe7af7ed1885cbf95e7149696601367dca788177
Bozidar <bozidar22112003@gmail.com>
2025-03-11 17:20:18 +0100

    addes Controllers

---
+++ StambenaZajednica/Controllers/FinansijeController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StambenaZajednica.Data.RepositoryInterfaces;
+using StambenaZajednica.Models;
+
+namespace StambenaZajednica.Controllers
+{
+    public class FinansijeController : Controller
+    {
+        private readonly IFinansijeRepository _finansijeRepo;
+
+        public FinansijeController(IFinansijeRepository finansijeRepo)
+        {
+            _finansijeRepo = finansijeRepo;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var racuni = await _finansijeRepo.GetAllAsync();
+            return View(racuni);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Finansije racun)
+        {
+            if (ModelState.IsValid)
+            {
+                await _finansijeRepo.AddAsync(racun);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(racun);
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var racun = await _finansijeRepo.GetByIdAsync(id);
+            if (racun == null) return NotFound();
+            return View(racun);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, Finansije racun)
+        {
+            if (id != racun.Id) return BadRequest();
+
+            if (ModelState.IsValid)
+            {
+                await _finansijeRepo.UpdateAsync(racun);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(racun);
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var racun = await _finansijeRepo.GetByIdAsync(id);
+            if (racun == null) return NotFound();
+            return View(racun);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _finansijeRepo.DeleteAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ StambenaZajednica/Controllers/StanController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StambenaZajednica.Data.RepositoryInterfaces;
+using StambenaZajednica.Models;
+
+namespace StambenaZajednica.Controllers
+{
+    public class StanController : Controller
+    {
+        private readonly IStanRepository _stanRepo;
+
+        public StanController(IStanRepository stanRepo)
+        {
+            _stanRepo = stanRepo;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var stanovi = await _stanRepo.GetAllAsync();
+            return View(stanovi);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Stan stan)
+        {
+            if (ModelState.IsValid)
+            {
+                await _stanRepo.AddAsync(stan);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(stan);
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var stan = await _stanRepo.GetByIdAsync(id);
+            if (stan == null) return NotFound();
+            return View(stan);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, Stan stan)
+        {
+            if (id != stan.Id) return BadRequest();
+
+            if (ModelState.IsValid)
+            {
+                await _stanRepo.UpdateAsync(stan);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(stan);
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var stan = await _stanRepo.GetByIdAsync(id);
+            if (stan == null) return NotFound();
+            return View(stan);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _stanRepo.DeleteAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
