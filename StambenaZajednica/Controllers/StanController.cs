43f688960cb03fa8d2b3e42ad17820af7c1b0209
Bozidar <bozidar22112003@gmail.com>
2025-09-05 18:26:07 +0200

    update pin stan/stanar

--- StambenaZajednica/Controllers/StanController.cs
+++ StambenaZajednica/Controllers/StanController.cs
@@ -2,16 +2,18 @@
 using StambenaZajednica.Data.RepositoryInterfaces;
 using StambenaZajednica.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 
 namespace StambenaZajednica.Controllers
 {
     public class StanController : Controller
     {
         private readonly IStanRepository _stanRepo;
-
-        public StanController(IStanRepository stanRepo)
+        private readonly UserManager<ApplicationUser> _userManager;
+        public StanController(IStanRepository stanRepo, UserManager<ApplicationUser> userManager)
         {
             _stanRepo = stanRepo;
+            _userManager = userManager;
         }
 
         // 📌 Prikaz svih stanova (dostupno samo za upravnika)
@@ -19,7 +21,25 @@ namespace StambenaZajednica.Controllers
         public async Task<IActionResult> Index()
         {
             var stanovi = await _stanRepo.GetAllAsync();
+            var stanari = await _userManager.GetUsersInRoleAsync("Stanar");
+            ViewBag.Stanari = await _userManager.GetUsersInRoleAsync("Stanar");
             return View(stanovi);
+        }
+
+        [Authorize(Roles = "Upravnik")]
+        [HttpPost]
+        public async Task<IActionResult> DodeliStanara(int stanId, string stanarId)
+        {
+            var stan = await _stanRepo.GetByIdAsync(stanId);
+            if (stan == null) return NotFound();
+
+            var stanar = await _userManager.FindByIdAsync(stanarId);
+            if (stanar == null) return NotFound();
+
+            stan.StanarId = stanar.Id;
+            await _stanRepo.UpdateAsync(stan);
+
+            return RedirectToAction(nameof(Index));
         }
 
         // 📌 Prikaz forme za kreiranje stana (samo za upravnika)
